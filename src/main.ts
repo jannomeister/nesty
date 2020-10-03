1f1340aa8956df6ae13d6948ecf612bd4e558d64
Janno De Jesus <jannotabamo@gmail.com>
2020-10-03 09:17:08 +0800

    feat: added and setup swagger (#11)
    * chore(package): added swagger package
    
    * feat(swagger): setup swaggeR

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+
+const initSwagger = app => {
+  const options = new DocumentBuilder()
+    .setTitle("API Docs")
+    .setVersion("0.0.1")
+    .setDescription("These are all the available APIs")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup("api-docs", app, document);
+};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  initSwagger(app);
+
   await app.listen(3000);
 }
 bootstrap();
