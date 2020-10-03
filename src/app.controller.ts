1b5a9562ea7be4186b764712e332c9ea090b0d62
Janno Tabamo <janno@hyperstacksinc.com>
2020-10-03 09:15:53 +0800

    feat(swagger): setup swaggeR

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from "@nestjs/common";
-import { AppService } from "./app.service";
+import { Controller, Get, Redirect } from "@nestjs/common";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor() {
+    //
+  }
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Redirect("api-docs")
+  init(): boolean {
+    return true;
   }
 }
