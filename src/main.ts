fb13b7d6f2b649d094fc1cdd30eb339395410e0f
Janno Tabamo <janno@hyperstacksinc.com>
2020-10-03 09:33:33 +0800

    feat: setup express security

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,24 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import * as helmet from "helmet";
+import * as rateLimit from "express-rate-limit";
+
+const initCors = () => {
+  const validateOrigin = (origin, next) => {
+    // TODO: handle different environments
+    next(null, true);
+  };
+
+  return { origin: validateOrigin, credentials: true };
+};
+
+const initHelmet = () => {
+  return helmet({
+    referrerPolicy: { policy: "no-referrer" },
+  });
+};
 
 const initSwagger = app => {
   const options = new DocumentBuilder()
@@ -14,7 +32,17 @@ const initSwagger = app => {
 };
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.enableCors(initCors());
+  app.use(initHelmet());
+  app.set("trust proxy", 1);
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 100, // limit each IP to 100 requests per windowMs
+    }),
+  );
 
   initSwagger(app);
 
